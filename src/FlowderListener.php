a610a0582daf98a810d21eb59157a8888c363498
Joe Haines <hello@joehaines.co.uk>
2017-04-22 16:00:42 +0100

    Update Flowder & allow manual loading

--- src/FlowderListener.php
+++ src/FlowderListener.php
@@ -15,6 +15,11 @@ final class FlowderListener extends BaseTestListener
     private static $flowder;
 
     /**
+     * @var mixed
+     */
+    private static $thingToLoad;
+
+    /**
      * Initialise the Flowder instance
      *
      * We only need this until https://github.com/sebastianbergmann/phpunit/issues/1873
@@ -22,11 +27,13 @@ final class FlowderListener extends BaseTestListener
      * and injected into the constructor
      *
      * @param Flowder $flowder
+     * @param mixed $thingToLoad
      * @return void
      */
-    public static function bootstrap(Flowder $flowder)
+    public static function bootstrap(Flowder $flowder, $thingToLoad)
     {
         static::$flowder = $flowder;
+        static::$thingToLoad = $thingToLoad;
     }
 
     /**
@@ -35,9 +42,10 @@ final class FlowderListener extends BaseTestListener
      *
      * @param Flowder|null $flowder
      */
-    public function __construct(Flowder $flowder = null)
+    public function __construct(Flowder $flowder = null, $thingToLoad = null)
     {
         static::$flowder = static::$flowder ?: $flowder;
+        static::$thingToLoad = static::$thingToLoad ?: $thingToLoad;
     }
 
     /**
@@ -49,9 +57,22 @@ final class FlowderListener extends BaseTestListener
      */
     public function startTest(PHPUnit_Framework_Test $test)
     {
-        $this->checkIsInitialised();
+        static::checkIsInitialised();
 
-        static::$flowder->loadFixtures();
+        static::$flowder->loadFixtures(static::$thingToLoad);
+    }
+
+    /**
+     * Load fixtures manually rather than via the PHPUnit Listener
+     *
+     * @param mixed $thingToLoad
+     * @return void
+     */
+    public static function loadFixtures($thingToLoad)
+    {
+        static::checkIsInitialised();
+
+        static::$flowder->loadFixtures($thingToLoad);
     }
 
     /**
@@ -60,9 +81,9 @@ final class FlowderListener extends BaseTestListener
      * @return void
      * @throws LogicException when the Flowder instance hasn't been created
      */
-    private function checkIsInitialised()
+    private static function checkIsInitialised()
     {
-        if (!isset(static::$flowder)) {
+        if (!isset(static::$flowder, static::$thingToLoad)) {
             throw new LogicException(
                 'FlowderListener must be configured by calling FlowderListener::bootstrap before any tests run'
             );
--- tests/Integration/FlowderListenerTest.php
+++ tests/Integration/FlowderListenerTest.php
@@ -39,15 +39,54 @@ class FlowderListenerTest extends TestCase
 
         FlowderListener::bootstrap(
             new Flowder(
-                __DIR__ . '/../data/example.php',
                 new PhpFileLoader(),
                 new SqliteTruncator($db),
                 new SqlitePersister($db)
-            )
+            ),
+            __DIR__ . '/../data/example.php'
         );
 
         $flowdception = new FlowderListener();
         $flowdception->startTest($this);
+
+        $statement = $db->prepare('SELECT * FROM example');
+        $statement->execute();
+
+        $actual = $statement->fetchAll();
+
+        $expected = [[
+            'one' => '1',
+            'two' => '2',
+            'three' => '3',
+            'four' => '4',
+        ]];
+
+        $this->assertSame($expected, $actual);
+    }
+
+    public function testItAllowsFixturesToBeLoadedManually()
+    {
+        $db = new PDO('sqlite::memory:');
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+        $db->exec('CREATE TABLE IF NOT EXISTS example (
+            one INT PRIMARY KEY,
+            two INT,
+            three INT,
+            four INT
+        )');
+
+        FlowderListener::bootstrap(
+            new Flowder(
+                new PhpFileLoader(),
+                new SqliteTruncator($db),
+                new SqlitePersister($db)
+            ),
+            'not a thing'
+        );
+
+        FlowderListener::loadFixtures(__DIR__ . '/../data/example.php');
 
         $statement = $db->prepare('SELECT * FROM example');
         $statement->execute();
